1c5541b8f6c664eeefede6a7da784cbf7222f5cb
Nicholas Robinson-Wall <nick@robinson-wall.com>
2023-12-02 17:36:25 +0000

    2023 Day 2

---
+++ src/main/scala/rw/nick/aoc/aoc2023/Dec2.scala
@@ -0,0 +1,73 @@
+package rw.nick.aoc.aoc2023
+
+import rw.nick.aoc.ParserUtils.numberChars
+import rw.nick.aoc.Solution
+
+object Dec2 {
+
+  enum Colour:
+    case Red, Green, Blue
+
+  case class Game(id: Int, selections: List[Map[Colour, Int]]) {
+
+    def isPossibleWithMaxAvailable(setup: Map[Colour, Int]): Boolean =
+      selections.forall { draw =>
+        setup.forall { case (col, maximum) =>
+          draw.getOrElse(col, 0) <= maximum
+        }
+      }
+
+    lazy val minimumRequirements: Map[Colour, Int] =
+      selections.fold(Map[Colour, Int]()) { case (collector, draw) =>
+        (draw.toSeq ++ collector.toSeq)
+          .groupMapReduce(_._1)(_._2)(math.max)
+      }
+
+    lazy val power: Int = minimumRequirements.values.product
+
+  }
+
+  def lineParser(line: String): Game = {
+    val (gameIDContainer, selectionContainer) = line.splitAt(line.indexOf(":") + 1)
+    val gameID = gameIDContainer.filter(numberChars.contains).toInt
+    val selections = selectionContainer
+      .split(';')
+      .map(_.split(',').map(singleSelectionParser))
+      .map(_.reduce(_ ++ _))
+    Game(gameID, selections.toList)
+  }
+
+  def singleSelectionParser(selectionContainer: String): Map[Colour, Int] = {
+    val (quantityStr, colourName) = selectionContainer.partition(numberChars.contains)
+    Map {
+      Colour.valueOf(colourName.strip.capitalize) -> quantityStr.toInt
+    }
+  }
+}
+
+abstract class Dec2 extends Solution("inputs/2023/dec2.txt")
+
+object Dec2Part1 extends Dec2 {
+  val Colour = Dec2.Colour
+  override def solution: Int = {
+    val gameSetup = Map(
+      Colour.Red -> 12,
+      Colour.Green -> 13,
+      Colour.Blue -> 14,
+    )
+    (for line <- input
+      yield Dec2.lineParser(line))
+      .filter(_.isPossibleWithMaxAvailable(gameSetup))
+      .map(_.id)
+      .sum
+  }
+}
+
+object Dec2Part2 extends Dec2 {
+  override def solution: Int = {
+    (for line <- input
+      yield Dec2.lineParser(line))
+      .map(_.power)
+      .sum
+  }
+}
---
+++ src/test/scala/rw/nick/aoc/aoc2023/TestDec2.scala
@@ -0,0 +1,98 @@
+package rw.nick.aoc.aoc2023
+
+import org.scalatest.funsuite.AnyFunSuite
+
+class TestDec2 extends AnyFunSuite {
+  val Colour = Dec2.Colour
+  val Red = Colour.Red
+  val Green = Colour.Green
+  val Blue = Colour.Blue
+  val Game = Dec2.Game
+
+  test("Test Day 2 single selection parser") {
+    assert(Dec2.singleSelectionParser("7 blue") == Map { Colour.Blue -> 7 } )
+  }
+
+  test("Test Day 2 line parser") {
+    val input: String = "Game 1: 2 red, 8 blue, 1 green; 9 green, 7 blue, 3 red"
+    val expected = Game(1, List(
+      Map(
+        Red -> 2,
+        Blue -> 8,
+        Green -> 1,
+      ),
+      Map(
+        Red -> 3,
+        Blue -> 7,
+        Green -> 9,
+      ),
+    ))
+    assert(Dec2.lineParser(input) == expected)
+  }
+
+  test("Day 2 game is possible") {
+    val game = Game(1, List(
+      Map(
+        Red -> 2,
+        Blue -> 8,
+        Green -> 1,
+      ),
+      Map(
+        Red -> 3,
+        Blue -> 7,
+        Green -> 9,
+      ),
+    ))
+    val isPossible = game.isPossibleWithMaxAvailable(
+      Map(
+        Red -> 12,
+        Blue -> 14,
+        Green -> 13,
+      ),
+    )
+    assert(isPossible)
+    val isNotPossible = game.isPossibleWithMaxAvailable(
+      Map(
+        Red -> 12,
+        Blue -> 14,
+        Green -> 8,
+      ),
+    )
+    assert(!isNotPossible)
+  }
+
+  test("Day 2 part 1 result") {
+    assert(Dec2Part1.solution == 2162)
+  }
+
+  test("Day 2 part 2 minimum requirements") {
+    val game = Game(1, List(
+      Map(
+        Red -> 2,
+        Blue -> 8,
+        Green -> 1,
+      ),
+      Map(
+        Red -> 12,
+        Blue -> 7,
+        Green -> 9,
+      ),
+      Map(
+        Red -> 3,
+        Blue -> 7,
+        Green -> 9,
+      ),
+    ))
+    val expectedMinimumRequirements = Map(
+      Red -> 12,
+      Blue -> 8,
+      Green -> 9,
+    )
+    assert(game.minimumRequirements == expectedMinimumRequirements)
+    assert(game.power == 12 * 8 * 9)
+  }
+
+  test("Day 2 Part 2 solution") {
+    assert(Dec2Part2.solution == 72513)
+  }
+}
