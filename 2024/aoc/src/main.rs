59dcb848fa63b7db70a31fbf38330dc4234f1007
Nicholas Robinson-Wall <nick@robinson-wall.com>
2024-12-05 22:12:50 +0000

    2024 Day 1 Part 1

---
+++ 2024/aoc/src/main.rs
@@ -0,0 +1,35 @@
+use std::cmp::{max, min};
+
+fn main() {
+    aoc2024_day1_part1()
+}
+
+fn aoc2024_day1_part1() {
+    let (mut list_a, mut list_b) = parse_input("input_day1_part1.txt");
+    list_a.sort();
+    list_b.sort();
+    let iterable = list_a.iter().zip(list_b.iter());
+    let result = iterable
+        .map(|(a, b)| max(a, b) - min(a, b) )
+        .sum::<usize>()
+    ;
+    println!("result: {}", result);
+}
+
+
+fn parse_line(line: String) -> (usize, usize) {
+    let a = &line[0..5];
+    let a = a.parse::<usize>().expect("failed to parse first number");
+    let b = &line[8..];
+    let b = b.parse::<usize>().expect("failed to parse second number");
+    (a, b)
+}
+
+fn parse_input(filename: &str) -> (Vec<usize>, Vec<usize>) {
+    std::fs::read_to_string(filename)
+        .expect("should be able to read from file")
+        .lines()
+        .map(String::from)
+        .map(parse_line)
+        .unzip()
+}
